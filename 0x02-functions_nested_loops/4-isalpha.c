1ce34e6fad26a076cc69115f106edfd8f489d1b6
Ayoade Micheal <ayoademic03@gmail.com>
2022-06-08 11:01:28 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,20 +1,16 @@
 #include "main.h"
 int _isalpha(int c)
 {
-	
-	if (c > 'a'  && c < 'z' )
+	if (c > 'a'  && c < 'z')
 	{
 		return (1);
 	}
-	
 	else if (c > 'A' && c < 'Z')
 	{
 		return (1);
 	}
-	
 	else
 	{
 		return (0);
 	}
-	
 }

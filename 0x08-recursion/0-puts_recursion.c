637e4ff2c16a694ee4ebc2951ec725ed09ff8ea5
Ayoade Micheal <ayoademic03@gmail.com>
2022-07-05 06:18:02 -0700

    correction

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+	        _puts_recursion(s + 1);
+	}
+	else
+	        _putchar('\n');
+	
+}

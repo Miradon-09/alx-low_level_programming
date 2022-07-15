ad7db2603bb4b1c7496fc71a619350116653a12b
Ayoade Micheal <ayoademic03@gmail.com>
2022-07-14 17:59:49 -0700

    correction task 2#

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - function taht prints all arguments it receives.
+ *@argc: argument count on the CDl interface.
+ *@argv: argument value on the CDL interface.
+ *Return: Always 0, success.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (arg > 0)
+	{
+		for (i = 0; i < argc; i++)
+			printf("%s\n", argv[i]);
+	}
+	return (0);
+}
